6480d3335f29a7bdc2d964b3a5d8c3a0f60f4923
JJ-Mac <jan.joos@joos-partner.ch>
2025-08-22 15:39:28 +0200

    Initial message updated

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ async def on_startup(app: Application):
         v = _read_version()
         short = v["sha"][:7]
         text = (
-            f"🚀 <b>Bot gestartet</b>\n"
+            f"🚀 <b>Drablibe-Bot wurde gestartet und/oder geupdatet!</b>\n"
             f"• Commit: <code>{short}</code> ({v['ref']})\n"
             f"• Autor: {v['author']}\n"
             f"• Build: {v['time']}\n"
